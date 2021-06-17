df22aa9a9dbc5ac7e47feeaf1f42c9c9074028eb
Nilesh Pandit <nilesh.pandit@joshsoftware.com>
2021-06-17 16:35:25 +0530

    added dashboard actions

--- src/redux/actions/dashboardAction/actions.js
+++ src/redux/actions/dashboardAction/actions.js
@@ -1,11 +1,54 @@
 import { DASHBOARD_CONSTANTS } from "./actionTypes";
 
-export const resetPosts = (payload) => ({
-  type: DASHBOARD_CONSTANTS.RESET_POSTS,
+const {
+  FETCH_POSTS,
+  FETCH_POSTS_SUCCESS,
+  FETCH_POSTS_ERROR,
+
+  FETCH_POST_DETAILS,
+  FETCH_POST_DETAILS_SUCCESS,
+  FETCH_POST_DETAILS_ERROR,
+
+  RESET_POSTS,
+  RESET_POST_DETAILS,
+} = DASHBOARD_CONSTANTS;
+
+export const fetchPosts = (payload) => ({
+  type: FETCH_POSTS,
+  payload,
+});
+
+export const fetchPostsSuccess = (payload) => ({
+  type: FETCH_POSTS_SUCCESS,
+  payload,
+});
+
+export const fetchPostsError = (payload) => ({
+  type: FETCH_POSTS_ERROR,
+  payload,
+});
+
+export const fetchPostDetails = (payload) => ({
+  type: FETCH_POST_DETAILS,
+  payload,
+});
+
+export const fetchPostDetailsSuccess = (payload) => ({
+  type: FETCH_POST_DETAILS_SUCCESS,
+  payload,
+});
+
+export const fetchPostDetailsError = (payload) => ({
+  type: FETCH_POST_DETAILS_ERROR,
+  payload,
+});
+
+export const resetPosts = () => ({
+  type: RESET_POSTS,
   payload: [],
 });
 
 export const resetPostDetails = () => ({
-  type: DASHBOARD_CONSTANTS.RESET_POST_DETAILS,
+  type: RESET_POST_DETAILS,
   payload: {},
 });
--- src/redux/sagas/dashboardSaga.js
+++ src/redux/sagas/dashboardSaga.js
@@ -1,36 +1,34 @@
 import { call, put, takeEvery } from "@redux-saga/core/effects";
 // import { getAllPosts, getPostById } from "../../apis/postApi";
 import { getAllPosts, getPostById } from "apis/postApi";
+import {
+  fetchPostDetailsError,
+  fetchPostDetailsSuccess,
+  fetchPostsError,
+  fetchPostsSuccess,
+} from "redux/actions/dashboardAction/actions";
 import { DASHBOARD_CONSTANTS } from "../actions/dashboardAction/actionTypes";
 
 // Worker saga will be fired on FETCH_POSTS actions
 function* fetchPosts(action) {
   try {
     const dashboardData = yield call(getAllPosts);
-    yield put({
-      type: DASHBOARD_CONSTANTS.FETCH_POSTS_SUCCESS,
-      payload: { posts: dashboardData.data },
-    });
+    yield put(fetchPostsSuccess, { posts: dashboardData.data });
   } catch (e) {
-    yield put({
-      type: DASHBOARD_CONSTANTS.FETCH_POSTS_ERROR,
-      payload: { error: e.message },
-    });
+    const { response } = e;
+    yield put(fetchPostsError, { error: response.data });
   }
 }
 
 function* fetchPostDetails(action) {
   try {
     const postDetailsResponse = yield call(getPostById, action.payload.id);
-    yield put({
-      type: DASHBOARD_CONSTANTS.FETCH_POST_DETAILS_SUCCESS,
-      payload: { postDetails: postDetailsResponse.data },
+    yield put(fetchPostDetailsSuccess, {
+      postDetails: postDetailsResponse.data,
     });
   } catch (e) {
-    yield put({
-      type: DASHBOARD_CONSTANTS.FETCH_POST_DETAILS_ERROR,
-      payload: { error: e.message },
-    });
+    const { response } = e;
+    yield put(fetchPostDetailsError, { error: response.data });
   }
 }
 
