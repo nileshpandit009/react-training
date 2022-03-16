9a509e7502a5ac28e638940c64c39d77df869b37
Nilesh Pandit <nileshpandit009@gmail.com>
2022-03-16 23:54:17 +0530

    temporary change. revert later.

--- src/apis/taigaHelper.js
+++ src/apis/taigaHelper.js
@@ -25,5 +25,6 @@ export const getAuthHeader = () => {
     const user = JSON.parse(loggedUser);
     return { Authorization: `Bearer ${user.auth_token}` };
   }
+  console.log("master branch");
   return {};
 };
